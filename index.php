bb92fa0ef56a42834686f370f372c1f2b6fc00fc
ajaishakya <80806815+ajaishakya@users.noreply.github.com>
2021-03-23 19:10:06 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login | MBA Admission</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="resource/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="resource/style.css"/>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker3.min.css"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+   
+    <div class="container" >
+        <div class="xl-gap"></div>
+        <div class="row myrow ">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-4 reg_container bg-light">
+                <div class="reg_header">
+                    <h3>Step 1 : Registration</h3>
+                </div>
+                <div style="min-height:500px;" class="d-flex justify-content-center mt-5" >
+                    <div style="width:280px;">
+                        <form method="post" action="">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" name="first_name" autocomplete="on" placeholder="First Name" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" name="last_name" autocomplete="on" placeholder="Last Name" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control form-control-sm" name="mobile" autocomplete="on" placeholder="Mobile Number" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="email" autocomplete="on" placeholder="Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="uremail" autocomplete="on" placeholder="Re-type Email" required>
+                            </div>
+                            <div class="form-group">
+                                <select name="gender"class="form-control form-control-sm">
+                                    <option value="" disabled selected>Gender</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="date" class="form-control form-control-sm" name="dob" autocomplete="off" placeholder="Born on" required>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" value="Register" name="submit1"/>
+                            </div>
+                        </form>
+                        <div class="text-center">Click <a href="https://mbainterview.apexcollege.edu.np">here</a> to fill MBA Interview Form.</div>
+                        <img src="resource/logo.png" class="mt-3">
+                    </div>
+                </div>
+                <div class="xl-gap"></div>
+
+            </div>
+
+            <div class="col-lg-4 reg_container bg-light">
+                <div class="reg_header">
+                    <h3>Step 2 : Sign In</h3>
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <div style="width:280px;">
+                        <form>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="email" autocomplete="off" placeholder="Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-sm" name="password" autocomplete="off" placeholder="Password" required>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" value="Login" name="submit2">
+                            </div>
+                        </form>
+                        <div class="text-center">
+                            <img class="img-logo" src="resource/MBAmorning.png">
+                            <img class="img-logo ml-4" src="resource/MBAevening.png">
+                        </div>
+                        <div class="text-center mt-5">Click <a href="https://mbainterview.apexcollege.edu.np">here</a> to fill MBA Interview Form.</div>
+                        <div class="text-center mt-2">Click <a href="https://admissions.apexcollege.edu.np/">here</a> to fill MBA Interview Form.</div>
+                        <img src="resource/logo.png" class="mt-4">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="xl-gap"></div>
+        <div class="xl-gap"></div>
+    </div>
+
+</body>
+</html>
