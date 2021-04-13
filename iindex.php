dff0b6289ed83fc28c19245a188cd2244be50d65
ajaishakya <80806815+ajaishakya@users.noreply.github.com>
2021-04-13 11:14:02 +0545

    Add files via upload
    Working on functions after login

---
+++ iindex.php
@@ -0,0 +1,125 @@
+<?php
+    include 'class.php';
+    $user = new User();
+
+    if(isset($_POST["submit1"]))
+    {
+        $register = $user->reg_user($_POST);
+        if($register==1)
+        {
+            echo "<p>Please check your mail for the password and to verify your Account(IF not also check in SPAM mail).</p>";
+        }
+        else
+        {
+            echo"<p>Database Error, Please Contact Administration.</p>";
+        }
+    }
+
+    if(isset($_POST["submit2"]))
+    {
+        $user->check_login($_POST);
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login | MBA Admission</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="resource/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="resource/style.css"/>
+    <link rel="stylesheet" type="text/css" href="https://unpkg.com/bootstrap-datepicker@1.9.0/dist/css/bootstrap-datepicker3.min.css"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container" >
+        <div class="xl-gap"></div>
+        <div class="row myrow ">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-4 reg_container bg-light">
+                <div class="reg_header">
+                    <h3>Step 1 : Registration</h3>
+                </div>
+                <div style="min-height:500px;" class="d-flex justify-content-center mt-5" >
+                    <div style="width:280px;">
+                        <form method="post" action="" id="register">
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" name="first_name" autocomplete="on" placeholder="First Name" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-sm" name="last_name" autocomplete="on" placeholder="Last Name" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="tel" class="form-control form-control-sm" name="mobile" autocomplete="on" placeholder="Mobile Number" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="email" autocomplete="on" placeholder="Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="uremail" autocomplete="on" placeholder="Re-type Email" required>
+                            </div>
+                            <div class="form-group">
+                                <select name="gender"class="form-control form-control-sm">
+                                    <option value="" disabled selected>Gender</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="date" class="form-control form-control-sm" name="dob" autocomplete="off" placeholder="Born on" required>
+                            </div>
+                             <input type="hidden" id="sid" name="session_id" value="1">
+                            <div class="form-group text-center">
+                                <input type="submit" value="Register" name="submit1">
+                            </div>
+                        </form>
+                        <div class="text-center">Click <a href="https://mbainterview.apexcollege.edu.np">here</a> to fill MBA Interview Form.</div>
+                        <img src="resource/logo.png" class="mt-3">
+                    </div>
+                </div>
+                <div class="xl-gap"></div>
+
+            </div>
+
+            <div class="col-lg-4 reg_container bg-light">
+                <div class="reg_header">
+                    <h3>Step 2 : Sign In</h3>
+                </div>
+                <div class="d-flex justify-content-center mt-5">
+                    <div style="width:280px;">
+                        <form method="POST" action="" >
+                            <div class="form-group">
+                                <input type="email" class="form-control form-control-sm" name="email" autocomplete="on" placeholder="Email" required>
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control form-control-sm" name="password" autocomplete="on" placeholder="Password" required>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" value="Login" name="submit2">
+                            </div>
+                        </form>
+                        <div class="text-center">
+                            <img class="img-logo" src="resource/MBAmorning.png">
+                            <img class="img-logo ml-4" src="resource/MBAevening.png">
+                        </div>
+                        <div class="text-center mt-5">Click <a href="https://mbainterview.apexcollege.edu.np">here</a> to fill MBA Interview Form.</div>
+                        <div class="text-center mt-2">Click <a href="https://admissions.apexcollege.edu.np/">here</a> to fill MBA Interview Form.</div>
+                        <img src="resource/logo.png" class="mt-4">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="xl-gap"></div>
+        <div class="xl-gap"></div>
+    </div>
+
+</body>
+</html>
---
+++ student.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registration Form | MBA</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <style>
+        a{
+            color:black;
+        }
+    </style>
+</head>
+
+<body>
+<!-- Header -->
+    <div style="height:100px" class="bg-dark d-flex align-items-center ">
+        <div class="container-fluid"  style="width:1230px;">
+            <div class="row">
+                <div class="col-11">
+                    <img src="resource/logo.png">
+                </div>
+                <div class="col d-flex align-items-center justify-content-end">
+                    <button type="button" class="btn btn-sm" style="background-color:rgb(115, 15, 14);color:lightgrey;">Logout</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Body -->
+    <div class="container-fluid d-flex justify-content-center">
+        <div class="row bg-light mt-4 mb-4 shadow-lg" style="height:800px; width:1200px;">
+            <div class="col-3" style="border-right:1px solid lightgrey" >
+                  <!-- Nav tabs -->
+                <ul class="nav nav-bar flex-column">
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link active" href="#welcome">Welcome</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#choose">Choose Program</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#personal">Personal Information</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#address">Address Information</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#family">Family Details</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#academic">Academic Information</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#work">Work and other Professional Experiences</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#achievements">Achievements</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#career">statement of purpose</a></li>
+                    <li class="nav-item"><a data-toggle="tab" class="nav-link" href="#decleration">Declaration</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                 <!-- Tab panes -->
+                <div class="tab-content">
+                    <div id="welcome" class="container tab-pane active"><br>
+                        <h3>Online Admissions Application Instructions</h3>
+                        <p>Welcome to Apex College's online application for Graduate Admission. We are happy for your interest for pursuing higher 
+                        education at Apex, and it is our goal to make the admission process as quick and easy as possible. As you begin your 
+                        application, there are a few things you should know about the online application process.</p>
+
+                        <ul>
+                        <li>Once you log in with your new account, you will receive an Email with password for your new Apex admission account in your E-mail ID.</li>
+                        <li>There are some boxes where you are required to fill up the required information. Your failure to fill up the boxes will result into error.</li>
+                        <li>If you have to leave your application without completing, you may save it on our server by clicking the "Save" button at the bottom of each page. You can later view or edit your application.</li>
+                        <li>When you are done with your online application, you must click the "Save" button at the bottom of the page.</li>
+                        <li>After submission of online application, you need to submit following documents at Apex Admission Council for verification and documentation before deadline date.</li>
+                            <ul>
+                                <li>Two passport size photos.</li>
+                                <li>Photocopy of Mark sheet of SLC</li>
+                                <li>Photocopy of Character Certificate of SLC</li>
+                                <li>Photocopy of Transcript of + 2</li>
+                                <li>Photocopy of Character Certificate of +2</li>
+                                <li>Photocopy of Transcript of Bachelor's Degree or grade sheets/mark sheets of all years/semesters</li>
+                                <li>Photocopy of Character Certificate of Bachelor's Degree</li>
+                                <li>Photocopy of Citizenship</li>
+                            </ul>
+                        </ul>
+                        <br>
+                        <p>Our Admission Officer will clarify all the application process by phone, or by Email.<br>
+                        Please contact us at:<a href="admissions@apexcollege.edu.np"> admissions@apexcollege.edu.np</a> or 01-4467922, 01-2278841.</p>
+                    </div>
+
+                    <div id="choose" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="personal" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="address" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="family" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="academic" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="work" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="achievements" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="career" class="container tab-pane"><br>
+                    </div>
+
+                    <div id="decleration" class="container tab-pane"><br>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+
+<!-- Footer -->
+    <div style="height:100px" class="bg-dark d-flex justify-content-center align-items-center">
+        <div class="text-center text-white " >
+            © Copyright 2017. All Rights Reserved.<br>Apex College
+        </div>
+    </div>
+    
+</body> 
+</html>
